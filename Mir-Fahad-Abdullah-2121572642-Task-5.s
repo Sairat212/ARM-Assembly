ed244c60c0677d51dbcbbab0639764db0e138ced
Mir-Fahad-Abdullah <fahadabdullah67@gmail.com>
2024-12-03 17:23:47 +0600

    Initial Commit

---
+++ Mir-Fahad-Abdullah-2121572642-Task-5.s
@@ -0,0 +1,32 @@
+.equ sevenseg,0xff200020
+.equ sevenseg1,0xff200030
+.global _start
+_start:
+	
+loop:
+	LDR r1, =sevenseg1
+	LDR r2, =#0b0111110100000000
+	STR r2, [r1]
+	ROR r2,#8
+	STR r2, [r1]
+	ROR r2,#8
+	LDR r2, =#0b0000000000000000
+	STR r2, [r1]
+	LDR r1, =sevenseg
+	LDR r2, =#0b01111101
+	
+	ROR r2, #8
+	STR r2, [r1]
+	ROR r2, #8
+	STR r2, [r1]
+	ROR r2, #8
+	STR r2, [r1]
+	
+	ROR r2, #8
+	STR r2, [r1]
+	LDR r1, =sevenseg
+	LDR r2, =#0b00000000
+	STR r2, [r1]
+	ROR r2, #8
+	
+	b loop
